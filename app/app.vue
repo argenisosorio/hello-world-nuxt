b9a1e9be032342dc163214caf10cf53ecdcb758e
Argenis Osorio <argenisosorio580@hotmail.com>
2026-02-13 13:41:40 -0400

    Added rest of files

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>¡Hola Mundo desde Nuxt.js!</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  font-family: sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+h1 {
+  color: #00dc82; /* El color verde característico de Nuxt */
+}
+</style>
